0385323bdf9998795728c06d71d42084136fa68c
Abubakar Usman <abusmoh@gmail.com>
2018-09-06 15:31:20 -0400

    wip

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,5 @@
 class Expense < ApplicationRecord
+    # validates :entry_date, :description, :outflow, presence: true
+    # validates :description, length: { minimum: 2 }
+    # validates :outflow, numericality: { greater_than: 0 }
 end
--- app/models/income.rb
+++ app/models/income.rb
@@ -1,2 +1,5 @@
 class Income < ApplicationRecord
+    # validates :entry_date, :description, :inflow, presence: true
+    # validates :description, length: { minimum: 2 }
+    # validates :inflow, numericality: { greater_than: 0 }
 end
