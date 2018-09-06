be341eadb5681734d27acee9ffbc037a131b7d97
Abubakar Usman <abusmoh@gmail.com>
2018-09-05 23:48:57 -0400

    fix forms, budget calculations and styling

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,4 +4,21 @@ class ExpensesController < ApplicationController
     @expenses = Expense.all
   end
 
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    if @expense.save
+      flash[:notice] = "New expense added"
+      redirect_to root_path
+    end
+  end
+
+  private
+    def expense_params
+      params.require(:expense).permit(:entry_date, :description, :outflow)
+    end
+
 end
